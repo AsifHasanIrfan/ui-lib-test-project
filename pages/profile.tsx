f09d4eb9e0a4807bc6a4a8a59ea89144eb190b79
Asif Hasan Irfan <asifhasanirfan@gmail.com>
2021-08-07 19:23:24 +0600

    [Final code]

---
+++ pages/profile.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+//Material-ul imports
+import { styled } from '@material-ui/core/styles';
+
+
+const TableContainer = styled('div')(({ theme }) => ({
+  paddingLeft: theme.spacing(4),
+}));
+
+export default function Index() {
+  return (  
+    <TableContainer>
+      <h4>This is profile page dummy content.</h4>
+    </TableContainer>
+  );
+}
